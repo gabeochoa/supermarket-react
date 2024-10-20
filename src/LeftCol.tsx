8eb3e6304c92bc0156276b24ae5a2156017aaeb7
Gabe Ochoa <gabeochoa@meta.com>
2024-10-20 13:42:19 -0400

    left column is now a table

--- src/LeftCol.tsx
+++ src/LeftCol.tsx
@@ -6,37 +6,52 @@ function PlayerStats(props) {
   return <div className="">Money {data.money}</div>;
 }
 
+function IconButton(props) {
+  return <button className="btn btn-square btn-xs">{props.icon}</button>;
+}
+
 function ButtonUpIcon(props) {
-  return <button className="btn btn-square btn-xs">🔼</button>;
+  return <IconButton icon={'🔼'} />;
 }
 
 function ButtonDownIcon(props) {
-  return <button className="btn btn-square btn-xs">🔽</button>;
+  return <IconButton icon={'🔽'} />;
 }
 
 function InventoryItem(props) {
   return (
-    <div className="card card-compact" key={props.id}>
-      <div className="card-body">
+    <tr>
+      <td>{props.amount}</td>
+      <td>{props.name}</td>
+      <td>
         <div className="join">
-          <p>{props.amount}</p>
-          <p>{props.name}</p>
-          <p>${props.price}</p>
+          <p style={{ paddingRight: 12 }}>${props.price}</p>
           <ButtonUpIcon />
           <ButtonDownIcon />
         </div>
-      </div>
-    </div>
+      </td>
+    </tr>
   );
 }
 
 function Inventory(props) {
   const { inventory, ITEMS } = useContext(DataContext);
   return (
-    <div className="">
-      {inventory.map((item) => (
-        <InventoryItem {...item} {...ITEMS[item.item_id]} />
-      ))}
+    <div className="overflow-x-auto">
+      <table className="table table-zebra">
+        <thead>
+          <tr>
+            <th>amt</th>
+            <th>item</th>
+            <th>price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {inventory.map((item) => (
+            <InventoryItem {...item} {...ITEMS[item.item_id]} />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
